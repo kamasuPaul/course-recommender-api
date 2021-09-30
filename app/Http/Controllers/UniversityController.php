8c3b937796696ead1d324043367bec900fd40f36
Kamasu Paul <kamasupaul@gmail.com>
2021-09-30 13:25:26 +0300

    -- add university resource

---
+++ app/Http/Controllers/UniversityController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\university;
+use Illuminate\Http\Request;
+
+class UniversityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\university  $university
+     * @return \Illuminate\Http\Response
+     */
+    public function show(university $university)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\university  $university
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, university $university)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\university  $university
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(university $university)
+    {
+        //
+    }
+}
