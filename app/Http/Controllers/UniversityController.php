9f637f5ce4a0c9a63ecc96097bbd347dbe9076d7
Kamasu Paul <kamasupaul@gmail.com>
2021-10-03 20:48:30 +0300

    -- add creating of a university

--- app/Http/Controllers/UniversityController.php
+++ app/Http/Controllers/UniversityController.php
@@ -29,23 +29,22 @@ class UniversityController extends Controller
         $validated = $this->validate($request, [
             'name' => 'required|string|max:255',
             'short_name' => 'string|max:255',
-            'address' => 'required|string|max:255',
-            'city' => 'required|string|max:255',
+            'address' => 'string|max:255',
+            'city' => 'string|max:255',
             'zip' => 'string|max:255',
-            'phone' => 'required|string|max:255',
+            'phone' => 'string|max:255',
             'fax' => 'string|max:255',
-            'email' => 'required|string|max:255',
-            'website' => 'required|string|max:255',
-            'logo' => 'required|string|max:255',
-            'description' => 'required|string|max:255',
-            'mission' => 'required|string|max:255',
-            'vision' => 'required|string|max:255',
-            'motto' => 'required|string|max:255',
-            'affiliation' => 'required|string|max:255',
-            'accreditation' => 'required|string|max:255',
-            'accreditation_date' => 'required|string|max:255',
-            'accreditation_expiration' => 'required|string|max:255',
-            'accreditation_authority' => 'required|string|max:255'
+            'email' => 'string|max:255',
+            'website' => 'string|max:255',
+            'logo' => 'string|max:255',
+            'description' => 'string|max:255',
+            'mission' => 'string|max:255',
+            'vision' => 'string|max:255',
+            'motto' => 'string|max:255',
+            'affiliation' => 'string|max:255',
+            'accreditation_date' => 'string|max:255',
+            'accreditation_expiration' => 'string|max:255',
+            'accreditation_authority' => 'string|max:255'
         ]);
         //store the model in the database
         $university = new University;
@@ -64,7 +63,6 @@ class UniversityController extends Controller
         $university->vision = $request->vision;
         $university->motto = $request->motto;
         $university->affiliation = $request->affiliation;
-        $university->accreditation = $request->accreditation;
         $university->accreditation_date = $request->accreditation_date;
         $university->accreditation_expiration = $request->accreditation_expiration;
         $university->accreditation_authority = $request->accreditation_authority;
--- database/migrations/2021_09_30_102034_create_universities_table.php
+++ database/migrations/2021_09_30_102034_create_universities_table.php
@@ -15,6 +15,30 @@ class CreateUniversitiesTable extends Migration
     {
         Schema::create('universities', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('short_name')->nullable(true);
+            $table->string('description')->nullable(true);
+            $table->string('city')->nullable(true);
+            $table->string('address')->nullable(true);
+            $table->string('phone')->nullable(true);
+            $table->string('fax')->nullable(true);
+            $table->string('email')->nullable(true);
+            $table->string('website')->nullable(true);
+            $table->string('logo')->nullable(true);
+            $table->string('cover_image')->nullable(true);
+            $table->string('facebook')->nullable(true);
+            $table->string('twitter')->nullable(true);
+            $table->string('instagram')->nullable(true);
+            $table->string('youtube')->nullable(true);
+            $table->string('linkedin')->nullable(true);
+            $table->string('mission')->nullable(true);
+            $table->string('vision')->nullable(true);
+            $table->string('motto')->nullable(true);
+            $table->string('affiliation')->nullable(true);
+            $table->date('accreditation_date')->nullable(true);
+            $table->string('accreditation_expiration')->nullable(true);
+            $table->string('accreditation_authority')->nullable(true);
+
             $table->timestamps();
         });
     }
