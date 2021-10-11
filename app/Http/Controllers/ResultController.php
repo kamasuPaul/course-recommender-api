9b2c71b93cb831ac4a6801090be6aa654d9e7d18
Kamasu Paul <kamasupaul@gmail.com>
2021-10-11 23:24:08 +0300

    add results model, controller and migration

---
+++ app/Http/Controllers/ResultController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Result;
+use Illuminate\Http\Request;
+
+class ResultController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Result  $result
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Result $result)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Result  $result
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Result $result)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Result  $result
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Result $result)
+    {
+        //
+    }
+}
